551c2e9fed2220632afb082150cd3733661a1670
Shelby Solomon <shelby@shelbysolomon.net>
2020-08-30 18:53:46 -0500

    initial commit

---
+++ buildScripts/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import path from 'path'
+import webpack from 'webpack'
+import config from '../webpack.config.dev'
+
+/* eslint-disable no-console */
+
+const port = 3000
+const app = express()
+const compiler = webpack(config)
+
+app.use(require('webpack-dev-middleware') (compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'))
+})
+
+app.listen(port, (err) => {
+  if (err) return console.log(err)
+  console.log(`Server listening on port ${port}`)
+})
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,3 @@
+import chalk from 'chalk'
+
+console.log(chalk.green('Starting app in development mode...')) //eslint-disable-line no-console
